aca64e8cd54de3ec9ebbe6c315dafed01d549e3c
Subhash Chand <chandu.5a4@gmail.com>
2020-07-03 14:58:16 +0530

    session2 continuation

--- session2.js
+++ session2.js
@@ -1,51 +1,93 @@
-'use strict'
-//execution context
- //global 
- var a = 10;
+//IIFE - Immediately invoked function expression
+(function(){
 
- //follow camel casing for all the function
+// 'use strict'
+// //execution context
+//  //global 
+//  var a = 10;
 
- function addB(){  
-    //global variable pollution
-     a =50
-     b =30;
- }
- addB();
- console.log('The b Value is' ,b);
+//  //follow camel casing for all the function
 
-//asking from paraeant scope
- function askForSomething(){
-     // changing the value in paren scope
-     //money = '$100';
-     // creating one variable within the given scope.
-      var money='200$';
-     console.log('I asked for something adn it gave me', money);
-     //explicilty asking for something
-     //console.log('I asked for something adn it gave me', window.money);
-    //block scope
-    let isTrue = true;
-    while(isTrue){
-        //block scope
-         let money ='500$';
-        //function scope
-        //var money ='$500';
-        console.log('I asked for something in a block scope and it gave me', money);
-        isTrue= false;
-    }
-    console.log('I asked for something adn it gave me', money);
+//  function addB(){  
+//     //global variable pollution
+//      a =50
+//      b =30;
+//  }
+//  addB();
+//  console.log('The b Value is' ,b);
 
- }
+// //asking from paraeant scope
+//  function askForSomething(){
+//      // changing the value in paren scope
+//      //money = '$100';
+//      // creating one variable within the given scope.
+//       var money='200$';
+//      console.log('I asked for something adn it gave me', money);
+//      //explicilty asking for something
+//      //console.log('I asked for something adn it gave me', window.money);
+//     //block scope
+//     let isTrue = true;
+//     while(isTrue){
+//         //block scope
+//          let money ='500$';
+//         //function scope
+//         //var money ='$500';
+//         console.log('I asked for something in a block scope and it gave me', money);
+//         isTrue= false;
+//     }
+//     console.log('I asked for something adn it gave me', money);
 
- askForSomething();
+//  }
+
+//  askForSomething();
  //function
 
  //block
 
 //closures
 
+//you can have variables with same name in global scope
+
+//scope chain
+var a = 100;
+
+function closureExample(){
+    var a =10 ;
+
+    //unlike other programming languages javascript can return functions
+    return function (){
+        console.log(a);
+    }
+} 
+
+// unlike other programming languages javascript variables can store functions
+
+const closureFunction = closureExample(); 
+//value returned would be
+//functions without names are called anonymous functions
+// function (){
+//     console.log(a);
+// }
+
+closureFunction();
+
+// unlike other programming languages javascript can pass function references as parameters
+
+
+function executeClosure(callBack){
+    var a =200;
+    callBack();
+}
+
+executeClosure(closureFunction);
+
 //callbacks
 
 //setTimeout
+
+setTimeout(()=>{
+console.log("Print me after 5000 ms")
+},5000)
 
 //eventloop
 
@@ -53,5 +95,5 @@
 
 //es6 classes
 
-
+})()
 
